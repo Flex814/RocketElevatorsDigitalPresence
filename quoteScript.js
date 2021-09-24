930a620d6012dd733f774440a0dcd0c0aa14af29
Jacob Gomez <jakegomez814@hotmail.com>
2021-09-23 21:33:18 -0400

    Last update of 9/23/21

--- quoteScript.js
+++ quoteScript.js
@@ -1,1 +1,32 @@
 "use strict";
+//If else statement
+$("#buildingType").change(function () {
+  if ($(this).val() == "Residential") {
+    $("#residentialOptions").show();
+    $("#commercialOptions").hide();
+    $("#corporateOptions").hide();
+    $("#hybridOptions").hide();
+  } else if ($(this).val() == "Commercial") {
+    $("#commercialOptions").show();
+    $("#corporateOptions").hide();
+    $("#hybridOptions").hide();
+    $("#residentialOptions").hide();
+  } else if ($(this).val() == "Corporate") {
+    $("#corporateOptions").show();
+    $("#hybridOptions").hide();
+    $("#residentialOptions").hide();
+    $("#commercialOptions").hide();
+  } else if ($(this).val() == "Hybrid") {
+    $("#hybridOptions").show();
+    $("#residentialOptions").hide();
+    $("#commercialOptions").hide();
+    $("#corporateOptions").hide();
+    //Hide all options untill user selects building type.
+  } else {
+    $("#residentialOptions").hide();
+    $("#commercialOptions").hide();
+    $("#corporateOptions").hide();
+    $("#hybridOptions").hide();
+  }
+});
+$("#buildingType").trigger("change");
