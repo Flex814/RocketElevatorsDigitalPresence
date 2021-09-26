0d6cab5cbebe882102bc806046b2b1f479de6875
Jacob Gomez <jakegomez814@hotmail.com>
2021-09-26 16:17:36 -0400

    done debugging

--- quoteScript.js
+++ quoteScript.js
@@ -4,20 +4,59 @@
 $("#buildingType").change(function () {
   // Show only Residential Options
   if ($(this).val() == "Residential") {
-    $("#mainOptions").show();
+    $("#number-of-apartments").show();
     $("#number-of-floors").show();
+    $("#number-of-basements").show();
+    $("#number-of-parking-spots").hide();
+    $("#number-of-companies").hide();
+    $("#number-of-elevators").hide();
+    $("#number-of-corporations").hide();
+    $("#maximum-occupancy").hide();
+    $("#business-hours").hide();
     // Show only Commercial Options
   } else if ($(this).val() == "Commercial") {
-    $("#mainOptions").show();
+    $("#number-of-apartments").hide();
+    $("#number-of-floors").show();
+    $("#number-of-basements").show();
+    $("#number-of-parking-spots").show();
+    $("#number-of-companies").show();
+    $("#number-of-elevators").show();
+    $("#number-of-corporations").hide();
+    $("#maximum-occupancy").hide();
+    $("#business-hours").hide();
     // Show only Corporate Options
   } else if ($(this).val() == "Corporate") {
-    $("#mainOptions").show();
+    $("#number-of-apartments").hide();
+    $("#number-of-floors").show();
+    $("#number-of-basements").show();
+    $("#number-of-parking-spots").show();
+    $("#number-of-companies").hide();
+    $("#number-of-elevators").hide();
+    $("#number-of-corporations").show();
+    $("#maximum-occupancy").show();
+    $("#business-hours").hide();
     // Show only Hybrid Options
   } else if ($(this).val() == "Hybrid") {
-    $("#mainOptions").show();
+    $("#number-of-apartments").hide();
+    $("#number-of-floors").show();
+    $("#number-of-basements").show();
+    $("#number-of-parking-spots").show();
+    $("#number-of-companies").show();
+    $("#number-of-elevators").hide();
+    $("#number-of-corporations").hide();
+    $("#maximum-occupancy").show();
+    $("#business-hours").show();
     //Hide all options until user selects building type.
   } else {
-    $("#options").hide();
+    $("#number-of-apartments").hide();
+    $("#number-of-floors").hide();
+    $("#number-of-basements").hide();
+    $("#number-of-parking-spots").hide();
+    $("#number-of-companies").hide();
+    $("#number-of-elevators").hide();
+    $("#number-of-corporations").hide();
+    $("#maximum-occupancy").hide();
+    $("#business-hours").hide();
   }
 });
 //Makes if else statements work on buildingType change.
