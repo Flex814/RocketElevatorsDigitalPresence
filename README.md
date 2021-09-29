0db63955720d9715cff6a08bcf89166a0e7ed412
Jacob Gomez <jakegomez814@hotmail.com>
2021-09-29 18:26:51 -0400

    residential part done

--- README.md
+++ README.md
@@ -44,3 +44,4 @@ Setting foundation for the javascript and jquery portions. Changing up quote.htm
 
 9/29/21:
 Home Stretch!!!! Show/Hide is working, just not pretty. Function for Residential is almost done, then will copy/paste and change it for the other building types. After that, it's debugging and launching website!
+(Update 1): finishing function for Residential to reuse for other building types. Getting stats to show up on screen for residential. Double checked values for Residential with the slack images and they are correct.
--- quoteScript.js
+++ quoteScript.js
@@ -30,24 +30,80 @@ var buildingSelect = function () {
 //make vars to store values of the options to use later with stated restrictions
 //if else statement for each product line selected?
 var residential = function () {
+  //var section
   //get nbAppartment
   var nbAppartment = parseFloat(document.getElementById("nbApartments").value);
-  console.log(nbAppartment);
+  console.log(nbAppartment + " number of apartments");
 
   //get nbFloor
   var nbFloor = parseFloat(document.getElementById("nbFloors").value);
-  console.log(nbFloor);
+  console.log(nbFloor + " number of floors");
 
-  //calculate nbAppartment/nbFloor and store in var AvgPerFloor
+  //calculate nbAppartment/nbFloor and store in var avgPerFloor
+  var avgPerFloor = Math.ceil(nbAppartment / nbFloor);
+  console.log(avgPerFloor + " average doors per floor");
 
   //calc AvgPerFloor/6 for the amount of elevators needed
-
-  //calc nbFloor / 20 to obtain nbElevatorShafts
+  var elevatorsNeeded = Math.ceil(avgPerFloor / 6);
+  console.log("elevators needed is " + elevatorsNeeded);
+  //calc nbFloor / 20 to obtain nbElevatorColumns
+  var nbElevatorColumns = Math.ceil(nbFloor / 20);
+  console.log("elevator columns is " + nbElevatorColumns);
   ////////////////////////////////////////////////////
+  //if else section
   //if standard is checked,
+  if (document.getElementById("standard").checked) {
+    console.log("standard is selected");
+    if (nbFloor > 20) {
+      elevatorsNeeded *= 2;
+    }
+
+    //price for elevator for standard
+    var elevatorPrice = sValue;
+    console.log("elevator price is " + elevatorPrice);
+    //if nbFloor > 20, multiply the (AvgPerFloor/6) by 2
+    //for total price of elevators
+    var totalPrice = parseFloat(elevatorsNeeded * sValue);
+    console.log("total price is " + totalPrice);
+
+    //installation fees
+    var iFees = totalPrice * standard;
+    console.log("install fee is " + iFees);
+
+    //final price
+    var finalPrice = totalPrice + iFees;
+    console.log("final price is " + finalPrice);
+
+    //////////////////////////////////////////////////////////
+    //if premium is checked,
+  } else if (document.getElementById("premium").checked) {
+    console.log("premium is selected");
+    if (nbFloor > 20) {
+      elevatorsNeeded *= 2;
+    }
+    var elevatorPrice = pValue;
+    var totalPrice = parseFloat(elevatorsNeeded * pValue);
+    var iFees = totalPrice * premium;
+    var finalPrice = totalPrice + iFees;
+  }
+  //if excelium is checked,
+  else if (document.getElementById("excelium").checked) {
+    console.log("excelium is selected");
+    if (nbElevatorColumns > 20) {
+      elevatorsNeeded *= 2;
+    }
+  }
+
   //if nbFloor > 20, multiply the (AvgPerFloor/6) by 2
-  //set price per elevator
-  //
+  //set unit price of each elevator
+  //set total price of elevators
+  //installation fees
+  //final price
+  document.getElementById("numberElevators").value = elevatorsNeeded;
+  document.getElementById("unitPrice").value = elevatorPrice;
+  document.getElementById("totalPrice").value = totalPrice;
+  document.getElementById("installationFees").value = iFees;
+  document.getElementById("finalPrice").value = finalPrice;
 };
 
 //////////////////////////////////////////////////////////
@@ -82,35 +138,35 @@ var hybrid = function () {};
 //alternate version of show/hide
 //longer, but easy to understand
 
+//   switch (true) {
+//     case $(this).val() == "Residential":
+//       $("#options").show();
+//       $("#number-of-companies").hide();
+//       $("#number-of-elevators").hide();
+//       $("#number-of-corporations").hide();
+//       $("#number-of-parking-spots").hide();
+//       $("#maximum-occupancy").hide();
+//       $("#business-hours").hide();
+//       break;
+//     case $(this).val() == "Commercial":
+//       $("#options").show();
+//       break;
+//     case $(this).val() == "Corporate":
+//       $("#options").show();
+//       break;
+//     case $(this).val() == "Hybrid":
+//       $("#options").show();
+//       $(".Uni").show();
+//       $("#number-of-apartments").hide();
+//       $("#number-of-elevators").hide();
+//       $("#number-of-corporations").hide();
+//       break;
+//     default:
+//       $("#options").hide();
+//   }
+//////////////////////////////////////////////////////////
+//If Else Version Alternate
 $("#buildingType").change(function () {
-  //   switch (true) {
-  //     case $(this).val() == "Residential":
-  //       $("#options").show();
-  //       $("#number-of-companies").hide();
-  //       $("#number-of-elevators").hide();
-  //       $("#number-of-corporations").hide();
-  //       $("#number-of-parking-spots").hide();
-  //       $("#maximum-occupancy").hide();
-  //       $("#business-hours").hide();
-  //       break;
-  //     case $(this).val() == "Commercial":
-  //       $("#options").show();
-  //       break;
-  //     case $(this).val() == "Corporate":
-  //       $("#options").show();
-  //       break;
-  //     case $(this).val() == "Hybrid":
-  //       $("#options").show();
-  //       $(".Uni").show();
-  //       $("#number-of-apartments").hide();
-  //       $("#number-of-elevators").hide();
-  //       $("#number-of-corporations").hide();
-  //       break;
-  //     default:
-  //       $("#options").hide();
-  //   }
-  //////////////////////////////////////////////////////////
-  //If Else Version Alternate
   if ($(this).val() == "Residential") {
     $("#options").show();
     $(".Uni").show();
